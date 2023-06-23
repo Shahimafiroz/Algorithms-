cb9e7cc7072d4ec27d19d4e6475e25eedae5c4ce
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-06-23 14:11:55 +0530

    Algorithms

---
+++ big-o.js
@@ -0,0 +1,11 @@
+const nemo = ["nemo"];
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("nemo found!!!");
+    }
+  }
+}
+
+findNemo(nemo);
