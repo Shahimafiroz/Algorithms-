34eaf5cef342ba4b2f111967fa8f7a689861b017
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-06-27 23:23:40 +0530

    nooo

--- Math/powerOf2.js
+++ Math/powerOf2.js
@@ -1,18 +1,33 @@
-// A simple method for this is to simply take the log of the number on base 2 and if you get an integer then the number is the power of 2
+// A simple method for this is to simply take the log of the number on base 2 and if you get an integer then the number is the power of 2 but this method wont surfice for the number 536870912 and above as Math.log function runs out of capacity
 
-function isPowerOf2(n) {
-  let a = Math.log(n);
-  let b = Math.log(2);
+// function isPowerOf2(n) {
+//   let a = Math.log(n);
+//   let b = Math.log(2);
 
-  if (Number.isInteger(a / b)) {
-    console.log("Yes it is not raised the power of 2.");
-    return "yes";
-  } else {
-    console.log("No , it is not raised to the power of 2");
-    return "no";
+//   if (Number.isInteger(a / b)) {
+//     console.log("Yes it is not raised the power of 2.");
+//     return "yes";
+//   } else {
+//     console.log("No , it is not raised to the power of 2");
+//     return "no";
+//   }
+// }
+
+// isPowerOf2(4);
+// isPowerOf2(6);
+// isPowerOf2(536870912);
+
+//// Iterative approach: Instead of using logarithms, consider an iterative approach where you repeatedly divide the number by 2 until it becomes 1 or an odd number. If it becomes an odd number before reaching 1, then it is not a power of two. ////
+function isPowerOfTwo(n) {
+  let x = 0;
+  while (2 ** x < n) {
+    x++;
   }
+  return 2 ** x === n;
 }
 
-isPowerOf2(4);
-isPowerOf2(6);
-isPowerOf2(536870912);
+// Consider the binary representation: Numbers that are powers of two have a distinct pattern in their binary representation. Try to observe this pattern and see if you can identify any unique properties that can help you detect powers of two.
+
+function binaryPow(n) {
+  let binFormat = Number.parseInt(n, 2);
+}
